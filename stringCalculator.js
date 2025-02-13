8d8daa985677399f14680a9689d4202ab7ec4419
nijeshraju <nijeshrajuk007@gmail.com>
2025-02-14 02:56:51 +0530

    refactor login for multiple values

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,8 +1,10 @@
 function add(numbers) {
     if (numbers === "") return 0;
 
-    const numArray = numbers.split(",").map(Number);
-    return numArray.reduce((sum, num) => sum + num, 0);
+    return numbers
+        .split(",")
+        .map(Number)
+        .reduce((sum, num) => sum + num, 0);
 }
 
 module.exports = add;

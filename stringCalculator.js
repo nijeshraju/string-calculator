9ad8a0b0274e59b05f1dc4b5128cd4a21466a19e
nijeshraju <nijeshrajuk007@gmail.com>
2025-02-14 03:18:49 +0530

    make pass for different delimiter

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,6 +1,17 @@
 function add(numbers) {
     if (numbers === "") return 0;
 
+    if (numbers.startsWith("//")) {
+        const parts = numbers.split("\n");
+        delimiter = new RegExp(parts[0].slice(2));
+        numbers = parts.slice(1).join("\n");
+
+        return numbers
+            .split(delimiter)
+            .map(Number)
+            .reduce((sum, num) => sum + num, 0);
+    }
+
     return numbers
         .split(/,|\n/)
         .map(Number)

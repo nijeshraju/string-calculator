a3fc9e3ce21962f95721e033e3b2567e89b03e75
nijeshraju <nijeshrajuk007@gmail.com>
2025-02-14 02:58:52 +0530

    make pass new line support

--- stringCalculator.js
+++ stringCalculator.js
@@ -2,7 +2,7 @@ function add(numbers) {
     if (numbers === "") return 0;
 
     return numbers
-        .split(",")
+        .split(/,|\n/)
         .map(Number)
         .reduce((sum, num) => sum + num, 0);
 }

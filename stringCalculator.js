72b3521da7534e6dfa88590dbe927e48ea63a636
nijeshraju <nijeshrajuk007@gmail.com>
2025-02-14 02:55:08 +0530

    make pass 2 number return sum

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,6 +1,8 @@
 function add(numbers) {
     if (numbers === "") return 0;
-    return Number(numbers);
+
+    const numArray = numbers.split(",").map(Number);
+    return numArray.reduce((sum, num) => sum + num, 0);
 }
 
 module.exports = add;

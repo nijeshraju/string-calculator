3d71af631590be3257c54bf1cc71c89d3dc22e56
nijeshraju <nijeshrajuk007@gmail.com>
2025-02-14 03:20:29 +0530

    refactor logic for delimiter

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,19 +1,16 @@
 function add(numbers) {
     if (numbers === "") return 0;
 
+    let delimiter = /,|\n/;
+    
     if (numbers.startsWith("//")) {
         const parts = numbers.split("\n");
         delimiter = new RegExp(parts[0].slice(2));
         numbers = parts.slice(1).join("\n");
-
-        return numbers
-            .split(delimiter)
-            .map(Number)
-            .reduce((sum, num) => sum + num, 0);
     }
 
     return numbers
-        .split(/,|\n/)
+        .split(delimiter)
         .map(Number)
         .reduce((sum, num) => sum + num, 0);
 }

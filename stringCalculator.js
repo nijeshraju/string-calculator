e13fdc4e95de555ad88a20278bf3295ce02f364b
nijeshraju <nijeshrajuk007@gmail.com>
2025-02-14 03:26:14 +0530

    make pass for negative numbers

--- stringCalculator.js
+++ stringCalculator.js
@@ -2,11 +2,18 @@ function add(numbers) {
     if (numbers === "") return 0;
 
     let delimiter = /,|\n/;
-    
+
     if (numbers.startsWith("//")) {
         const parts = numbers.split("\n");
         delimiter = new RegExp(parts[0].slice(2));
         numbers = parts.slice(1).join("\n");
+    }
+
+    const numArray = numbers.split(delimiter).map(Number);
+    const negatives = numArray.filter(n => n < 0);
+
+    if (negatives.length) {
+        throw new Error(`negative numbers not allowed ${negatives.join(",")}`);
     }
 
     return numbers
